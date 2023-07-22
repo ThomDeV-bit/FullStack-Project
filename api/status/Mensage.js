f25d9e596d00767f4b8a69bad433428ffad24472
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-07-22 12:44:33 -0300

    Feat class Mensage

---
+++ api/status/Mensage.js
@@ -0,0 +1,10 @@
+
+export class Mensage{
+    constructor(mensage , data , validate){
+        this.mensage = mensage,
+        this.data = data,
+        this.validate = validate
+    }
+       
+}
+
