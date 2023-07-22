2141c312cbe0863f7851d415a8c77c0ff7c8efd2
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-07-22 12:44:48 -0300

    feat addUser

--- api/controller/user.js
+++ api/controller/user.js
@@ -1,10 +1,41 @@
 import { db } from "../db.js"
+import { Mensage } from "../status/Mensage.js";
+
+
 
 export const getUsers = (_, res) => {
     const q = "SELECT * FROM users";
     db.query(q, (err, data) => {
         if (err) return res.json(err)
-        
+
         return res.status(200).json(data)
     })
+}
+
+export const addUser = async (req, res) => {
+    
+    const q = "INSERT INTO users (`user_name`,`mail`,`fone`) VALUES (?)";
+    const values =[
+        req.body.user_name,
+        req.body.mail,
+        req.body.fone]
+    
+
+    const validate = "SELECT FROM users WHERE user_name?"
+    const userValidadete = req.body.user_name
+    if (validate != userValidadete) {
+
+
+        try {
+            db.query(q, [values], (err) => {
+                if (err) {
+                    return res.json(err)
+                }
+                return res.status(200).json(new Mensage("User Create Sucefully", req.body , true))
+            })
+
+        } catch (error) {
+            return res.status(500).json(new Mensage("Unexpected Error", error, false))
+        }
+    }
 }
--- api/routes/users.js
+++ api/routes/users.js
@@ -1,8 +1,9 @@
 import express from "express"
-import {getUsers} from "../controller/user.js"
+import {addUser, getUsers} from "../controller/user.js"
 
 const userRouter = express.Router()
 
-userRouter.get('/',getUsers)
+userRouter.get('/find',getUsers)
+userRouter.post('/create', addUser)
 
 export default userRouter
