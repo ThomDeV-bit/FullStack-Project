06d4612ab62548fc5b06679d5c7882202cdf4482
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-07-25 10:20:54 -0300

    fix usercontroller import

--- api/routes/users.js
+++
@@ -1,9 +0,0 @@
-import express from "express"
-import {addUser, getUsers} from "../controller/user.js"
-
-const userRouter = express.Router()
-
-userRouter.get('/find',getUsers)
-userRouter.post('/create', addUser)
-
-export default userRouter
