afffa6b4508bdd976597c66f6a0e58df362588cf
ThomDeV-bit <120723852+ThomDeV-bit@users.noreply.github.com>
2023-07-25 10:20:19 -0300

    fix  userController import

--- api/index.js
+++ api/index.js
@@ -1,12 +1,12 @@
 import  express  from "express";
 import cors from "cors"
-import userRouter from "./routes/users.js";
+import UserController from "./routes/usersController.js"
 
 const app = express()
 
 app.use(express.json())
 app.use(cors())
 
-app.use('/user',userRouter)
+app.use(("/", UserController))
 
 app.listen(3001,()=>{console.log('Server running')})
